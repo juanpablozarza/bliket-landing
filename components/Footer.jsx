29f0a902740419d1fea0eb4383027b117a9a2636
JP <jpzarza@jpzarza.local>
2022-11-21 21:43:20 -0600

    Contact info changed

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
         <div className="w-full h-50 grid bg-primary-400 place-items-center mt-5 ">
             <div>
                 <h1 className="inline-block text-white text-xl font-bold my-5 ">Contáctanos:</h1>
-                <h1 className="inline-block text-white text-lg font-semibold ml-3 ">jpzarza@icloud.com</h1>
+                <h1 className="inline-block text-white text-lg font-semibold ml-3 ">admin@bliket.io</h1>
             </div>
         </div>
     );
